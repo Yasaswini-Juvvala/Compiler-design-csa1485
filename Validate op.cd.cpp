973a68e080fc1ce80f7d25ad9528281ec3a5d2d8
Yasaswini-Juvvala <112235169+Yasaswini-Juvvala@users.noreply.github.com>
2023-04-27 09:09:29 +0530

    Add files via upload

---
+++ Validate op.cd.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10, y = 5;
+    
+    printf("x + y = %d\n", x + y); 
+    printf("x - y = %d\n", x - y); 
+    printf("x * y = %d\n", x * y); 
+    printf("x / y = %d\n", x / y); 
+    printf("x %% y = %d\n", x % y); 
+    printf("++x = %d\n", ++x); 
+    printf("--y = %d\n", --y); 
+    
+    return 0;
+}
